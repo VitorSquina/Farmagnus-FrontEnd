e09a85b2f4c2de679738a18fb2497ffe5f7a83a7
Luiz-Felipe-Trindade <luiz.f.trindade0@gmail.com>
2025-04-20 17:05:15 -0300

    Finalizado a autenticação do usuario e a base da tela de login

--- Farmagnus-FrontEnd/src/App.jsx
+++ Farmagnus-FrontEnd/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import "./public/colors.module.css";
+import "./public/reset.css";
 import { Rotas } from "./Routes/Rotas";
 
 function App() {
--- Farmagnus-FrontEnd/src/Routes/Rotas.jsx
+++ Farmagnus-FrontEnd/src/Routes/Rotas.jsx
@@ -1,11 +1,22 @@
+// React
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+// Pages
 import { TesteComponentes } from "../pages/TesteComponentes/TesteComponentes";
+import { Login } from "../pages/Login/Login";
+//Context
+import { AuthProvider } from "../contexts/AuthProvider";
+// PrivateRoute
+import { PrivateRoute } from "./PrivateRoute";
+
 export const Rotas = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/teste" element={<TesteComponentes />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/teste" element={<TesteComponentes />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
