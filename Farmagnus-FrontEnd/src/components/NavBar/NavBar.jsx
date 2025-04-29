d5beaa368baf12d503fd29070df4bb5136fce269
Luiz-Felipe-Trindade <luiz.f.trindade0@gmail.com>
2025-04-29 17:07:32 -0300

    Criado o componente da NavBar

--- Farmagnus-FrontEnd/src/components/NavBar/NavBar.jsx
+++ Farmagnus-FrontEnd/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,52 @@
+//Styles
 import styles from "./NavBar.module.css";
+//React
+import React from "react";
+//React-Router-Dom
+import { NavLink } from "react-router-dom";
+//Context
+import { useAuth } from "../../contexts/AuthProvider";
+//React-Icons
+import { LuLayoutDashboard } from "react-icons/lu";
+import { BsBoxSeam } from "react-icons/bs";
+import { FiSmartphone } from "react-icons/fi";
+import { LuTruck } from "react-icons/lu";
+import { PiClockCounterClockwiseBold } from "react-icons/pi";
+import { FiLogOut } from "react-icons/fi";
 
 export const NavBar = () => {
-  return <nav></nav>;
+  const { logout } = useAuth();
+
+  return (
+    <nav className={styles.navContainer}>
+      <ul className={styles.navList}>
+        <NavLink className={styles.navItem} to={"/dashboard"}>
+          <LuLayoutDashboard size={20} />
+          <p>Dashboard</p>
+        </NavLink>
+        <NavLink className={styles.navItem} to={"/pedidos"}>
+          <BsBoxSeam size={20} />
+          <p>Pedidos</p>
+        </NavLink>
+        <NavLink className={styles.navItem} to={"/catalogo"}>
+          <FiSmartphone size={20} />
+          <p>Catálogo</p>
+        </NavLink>
+        <NavLink className={styles.navItem} to={"/entregas"}>
+          <LuTruck size={20} />
+          <p>Entregas</p>
+        </NavLink>
+        <NavLink className={styles.navItem} to={"/historico"}>
+          <PiClockCounterClockwiseBold size={20} />
+          <p>Histórico</p>
+        </NavLink>
+      </ul>
+      <div className={styles.logoutContainer}>
+        <button className={styles.logout} onClick={logout}>
+          <FiLogOut className={styles.icon} />
+          Sair
+        </button>
+      </div>
+    </nav>
+  );
 };
