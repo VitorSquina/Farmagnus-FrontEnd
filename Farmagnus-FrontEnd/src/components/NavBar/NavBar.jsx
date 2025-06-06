f027be825f6df384457668f51b61f5c12ddf9df8
Luiz-Felipe-Trindade <luiz.f.trindade0@gmail.com>
2025-06-06 14:46:04 -0300

    Alterado o navlink entregas da navbar para horarios

--- Farmagnus-FrontEnd/src/components/NavBar/NavBar.jsx
+++ Farmagnus-FrontEnd/src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ import { useAuth } from '../../contexts/Auth/useAuth';
 import { LuLayoutDashboard } from 'react-icons/lu';
 import { BsBoxSeam } from 'react-icons/bs';
 import { FiSmartphone } from 'react-icons/fi';
-import { LuTruck } from 'react-icons/lu';
+import { FaRegClock } from 'react-icons/fa6';
 import { PiClockCounterClockwiseBold } from 'react-icons/pi';
 import { FiLogOut } from 'react-icons/fi';
 
@@ -33,8 +33,8 @@ export const NavBar = () => {
           <p>Catálogo</p>
         </NavLink>
         <NavLink className={styles.navItem} to={'/entregas'}>
-          <LuTruck size={20} />
-          <p>Entregas</p>
+          <FaRegClock size={20} />
+          <p>Horários</p>
         </NavLink>
         <NavLink className={styles.navItem} to={'/historico'}>
           <PiClockCounterClockwiseBold size={20} />
